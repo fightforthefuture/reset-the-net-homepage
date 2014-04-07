2de51fcf08a6b0fcc5ad0bc0a4fa7c7d0131fdf6
Chris Antaki <ChrisAntaki@gmail.com>
2014-04-07 14:58:03 -0700

    The participant template has been added

--- grid.js
+++ grid.js
@@ -1,3 +1,34 @@
-if (location.hash === '#preview28') {
-    $('#grid').show();
+$(function() {
+
+
+
+if (location.hash !== '#preview28') {
+    return;
 }
+
+$('#grid').show();
+
+$.ajax({
+    url: '//reset-the-net-form-backend.herokuapp.com/users',
+    success: function(res) {
+        var participantsHtml = '';
+        res.forEach(function(user) {
+            var html = '<div id="user-' + user._id + '" class="slide">' +
+                '<meta name="user_id" content="' + user._id + '">' +
+                '<a target="_top" href="' + user.url + '">' +
+                '<img src="' + (user.image || '//fightforthefuture.github.io/reset-the-net-form/images/happycat.jpg') + '">' +
+                '</a>' +
+                '<p>' +
+                '<strong>' + user.name + '</strong> <span>' + user.description + '</span>' +
+                '</p>' +
+                '</div>';
+
+            participantsHtml += html;
+        });
+        $('#grid .participants').html(participantsHtml);
+    }
+});
+
+
+
+});
