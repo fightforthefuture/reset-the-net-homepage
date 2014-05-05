d280926081426196b1504c8d5d3a496ad684eb0c
Chris Antaki <ChrisAntaki@gmail.com>
2014-05-05 12:46:55 -0700

    I added functionality to the View More button

--- homepage.js
+++ homepage.js
@@ -9,28 +9,80 @@ if (location.hash !== '#preview28') {
     return;
 }
 
+// Here are some variables we'll want to see persist.
+var participants = [];
+var currentParticipantIndex = 0;
+
+// Show the Grid. Otherwise, being under development, it's hidden by default.
 $('#grid').show();
 
+// This is our HTML template method.
+function createParticipantHtml(user) {
+    var html = '<div id="user-' + user._id + '" class="slide">' +
+        '<meta name="user_id" content="' + user._id + '">' +
+        '<a target="_top" href="' + user.url + '">' +
+        '<img src="' + (user.image || '//fightforthefuture.github.io/reset-the-net-form/images/happycat.jpg') + '">' +
+        '</a>' +
+        '<p>' +
+        '<strong>' + user.name + '</strong> <span>' + user.description + '</span>' +
+        '</p>' +
+        '</div>';
+
+    return html;
+}
+
+function showNextGroupOfParticipants() {
+    // Define how many participants will form a group.
+    var groupSize = 5;
+
+    // Get the next group of participants.
+    var slice = participants.slice(currentParticipantIndex, currentParticipantIndex + groupSize);
+    
+    // Increment our participant index.
+    currentParticipantIndex += groupSize;
+
+    // Create new HTML.
+    var groupHtml = '<div>';
+    slice.forEach(function(user) {
+        groupHtml += createParticipantHtml(user);
+    });
+    groupHtml += '</div>';
+    var $group = $(groupHtml);
+
+    // Animate the group.
+    $group.css({
+        opacity: 0
+    }).animate({
+        opacity: 1
+    }, 432);
+
+    // Append new HTML.
+    $('#grid .participants').append($group);
+
+    // Hide the view more button, if we've seen everything.
+    if (currentParticipantIndex >= participants.length) {
+        $('#view-more-participants').remove();
+    }
+}
+
+// Fetch the participants.
 $.ajax({
     url: '//reset-the-net-form-backend.herokuapp.com/users',
     success: function(res) {
-        var participantsHtml = '';
-        res.forEach(function(user) {
-            var html = '<div id="user-' + user._id + '" class="slide">' +
-                '<meta name="user_id" content="' + user._id + '">' +
-                '<a target="_top" href="' + user.url + '">' +
-                '<img src="' + (user.image || '//fightforthefuture.github.io/reset-the-net-form/images/happycat.jpg') + '">' +
-                '</a>' +
-                '<p>' +
-                '<strong>' + user.name + '</strong> <span>' + user.description + '</span>' +
-                '</p>' +
-                '</div>';
+        // Show the View More button.
+        $('#view-more-participants').show();
 
-            participantsHtml += html;
-        });
-        $('#grid .participants').html(participantsHtml);
+        // Save the participants.
+        participants = res;
+
+        // Show the next couple of groups.
+        showNextGroupOfParticipants();
+        showNextGroupOfParticipants();
     }
 });
+
+// When the View More button is clicked, oblige.
+$('#view-more-participants').on('click', showNextGroupOfParticipants);
 
 
 
