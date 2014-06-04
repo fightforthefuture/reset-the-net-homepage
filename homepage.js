c4a408074ed1f907de6f9b18e9dcff7789cdda6b
Joel Monegro <jmonegro@gmail.com>
2014-06-03 22:24:33 -0400

    nav and smooth scroll

--- homepage.js
+++ homepage.js
@@ -1,4 +1,62 @@
+(function($) {
+    $.fn.goTo = function() {
+        $('html, body').animate({
+            scrollTop: $(this).offset().top + 'px'
+        }, 750);
+        return this; // for chaining...
+    }
+})(jQuery);
+
+
 $(function() {
+
+    var prev; //keep track of previous selected link
+    var isVisible= function(el){
+        el = $(el);
+        
+        if(!el || el.length === 0){
+            return false
+        };
+
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+        
+        var elemTop = el.offset().top;
+        var elemBottom = elemTop + el.height();
+        return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom));
+    }
+        
+    $(window).scroll(function(){
+        $('.navigation .navinn a').each(function(index, el){
+            el = $(el);
+            if(isVisible(el.attr('href'))){
+                if(prev){
+                    prev.removeClass('active');
+                }
+                el.addClass('active');
+                prev = el;
+                
+                //break early to keep highlight on the first/highest visible element
+                //remove this you want the link for the lowest/last visible element to be set instead
+                return false; 
+            }
+        });
+    });
+    
+    //trigger the scroll handler to highlight on page load
+    $(window).scroll();
+
+    $('.nav-item').click(function(e){
+        e.preventDefault();
+        var target = $(this).attr('href');
+        $(target).goTo();
+    })
+
+
+if (["#splash-screen", "#privacy-pack"].indexOf(window.location.hash) > -1) {
+    $("#splash-screen").goTo();
+};
+
 
 // Heroic video should video.
 $('.actions').on('click', function(e) {
@@ -138,7 +196,7 @@ $.ajax({
 });
 
 // When the View More button is clicked, oblige.
-$('#view-more-participants').on('click', showNextGroupOfParticipants);
+$('#view-more-participants').on('click', function(){showNextGroupOfParticipants();showNextGroupOfParticipants();});
 
 
 
