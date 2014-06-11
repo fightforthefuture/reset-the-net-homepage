b4fa17ffacaf4205ea976b4eb07a07a1a3836471
Chris Antaki <ChrisAntaki@gmail.com>
2014-06-11 13:00:01 -0700

    Updated weights

--- js/homepage.js
+++ js/homepage.js
@@ -107,7 +107,7 @@ function rotateOrganizations() {
         // American Civil Liberties Union
         "disclaimer": "<a href='http://aclu.org' target='_blank'>ACLU</a> will contact you about future campaigns. <a href='https://www.aclu.org/american-civil-liberties-union-privacy-statement' target='_blank'>Privacy policy</a>",
         "tag": "reset-the-net-aclu",
-        "weight": 20
+        "weight": 0
     }, {
         // OpenMedia
         "disclaimer": "<a href='http://openmedia.org' target='_blank'>OpenMedia</a> will contact you about future campaigns. <a href='https://openmedia.ca/privacy' target='_blank'>Privacy policy</a>",
@@ -117,7 +117,7 @@ function rotateOrganizations() {
         // Demand Progress
         "disclaimer": "<a href='http://demandprogress.org/' target='_blank'>Demand Progress</a> will contact you about future campaigns. <a href='http://www.demandprogress.org/privacy/' target='_blank'>Privacy policy</a>",
         "tag": "reset-the-net-demand-progress",
-        "weight": 50
+        "weight": 65
     }, {
         // Fight for the Future
         "disclaimer": "<a href='http://www.fightforthefuture.org/'>Fight for the Future</a> and <a href='http://www.thecenterforrights.org/'>Center for Rights</a> will contact you about future campaigns. <a href='http://www.fightforthefuture.org/privacy/'>Privacy Policy</a>",
@@ -132,7 +132,7 @@ function rotateOrganizations() {
         // CREDO
         "disclaimer": '<a href="credoaction.com/">CREDO</a> will contact you about future campaigns. <a href="http://www.credomobile.com/Misc/Privacy.aspx">Privacy Policy</a>',
         "tag": "reset-the-net-credo",
-        "weight": 30
+        "weight": 35
     }];
 
     function getScore(weight) {
